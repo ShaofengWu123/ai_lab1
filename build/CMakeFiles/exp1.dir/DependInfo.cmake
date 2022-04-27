4df1d61bd319153d508a9d36357f7aaab718e091
shaofeng-vlab <wsf123@mail.ustc.edu.cn>
2022-04-27 23:07:31 +0800

    clean build files

--- build/CMakeFiles/exp1.dir/DependInfo.cmake
+++ build/CMakeFiles/exp1.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/root/ai_lab1/AIexp1/main.cpp" "CMakeFiles/exp1.dir/main.cpp.o" "gcc" "CMakeFiles/exp1.dir/main.cpp.o.d"
+  "/root/ai_lab1/main.cpp" "CMakeFiles/exp1.dir/main.cpp.o" "gcc" "CMakeFiles/exp1.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links.
